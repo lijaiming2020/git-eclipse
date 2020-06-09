c30f1d9f773566b1a2bdf9c88f7f83a355a434d2
lijaiming2020 <1093861206@qq.com>
2020-06-09 15:12:22 +0800

    提交本地仓库

---
+++ git_project/src/git_project/UserDao.java
@@ -0,0 +1,11 @@
+package git_project;
+
+public class UserDao {
+	private int id;
+	private String password;
+	public void studentmodel(int id,String password){
+		this.id=id;
+		this.password=password;
+	}
+
+}
